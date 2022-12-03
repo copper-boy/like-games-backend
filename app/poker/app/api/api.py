1c8911c9da28af4c0c631143d9e82680d8215fd9
copper-boy <sermed512@gmail.com>
2022-12-03 22:50:31 +0300

    Add include routers in poker service

--- app/poker/app/api/api.py
+++ app/poker/app/api/api.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter
 
+from .v1 import router as v1_router
+from .v2 import router as v2_router
 from .v3 import router as v3_router
+from .ws import router as ws_router
+
 
 router = APIRouter()
+router.include_router(v1_router)
+router.include_router(v2_router)
 router.include_router(v3_router)
+router.include_router(ws_router)
